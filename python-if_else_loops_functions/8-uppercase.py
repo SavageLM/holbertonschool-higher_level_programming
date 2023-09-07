e0ca5f3096face537666e5bb73c6141aad99bbeb
SavageLM <nagol.egavas@gmail.com>
2023-09-07 12:15:37 -0700

    Rewrote 8-uppercase code to added a newline character

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def uppercase(str):
 
+    newstr = ""
     for i in str:
         character = ord(i)
         if 96 < character and character < 123:
             character = character - 32
-            print("{:c}".format(character), end='')
+            character = chr(character)
+            newstr += character
         else:
-            print("{:c}".format(character), end='')
+            character = chr(character)
+            newstr += character
+    print("{}\n".format(newstr))
