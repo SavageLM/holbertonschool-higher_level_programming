dea98e76efffbec5b848a966be2ec6a827604864
SavageLM <nagol.egavas@gmail.com>
2023-09-07 19:15:58 -0700

    Trying to add ane import guard

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == '__main__':
+    import sys
 
-if len(sys.argv) == 1:
-    print("0 arguments.")
-elif len(sys.argv) == 2:
-    print("{:d} argument:".format(len(sys.argv) - 1))
-else:
-    print("{:d} arguments:".format(len(sys.argv) - 1))
-for i in sys.argv:
-    if sys.argv.index(i) > 0:
-        print("{:d}: {:s}".format(sys.argv.index(i), i))
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv) - 1))
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+    for i in sys.argv:
+        if sys.argv.index(i) > 0:
+            print("{:d}: {:s}".format(sys.argv.index(i), i))
