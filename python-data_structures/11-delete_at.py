055520288b0ce5576ae0cc9408b92eb4618cdc0b
SavageLM <nagol.egavas@gmail.com>
2023-09-12 14:12:15 -0700

    Adjusted del function

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -5,5 +5,5 @@ def delete_at(my_list=[], idx=0):
         return my_list
     if idx > len(my_list):
         return my_list
-    new_list = del(my_list[idx])
+    new_list = del my_list[idx]
     return new_list
