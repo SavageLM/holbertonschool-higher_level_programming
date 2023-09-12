86c012a996b8830691cb383c2d360669637b2f8c
SavageLM <nagol.egavas@gmail.com>
2023-09-11 20:16:10 -0700

    Fixed secind for loop and what it was iterating throughb

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
 
     flag = 0
     for i in matrix:
-        for j in matrix:
+        for j in i:
             if flag != 0:
                 print(" ", end='')
             print("{:d}".format(j), end='')
