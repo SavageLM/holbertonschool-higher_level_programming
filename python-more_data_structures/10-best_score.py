fe7c976b0dc73cea25c235c33fb9257954548241
SavageLM <nagol.egavas@gmail.com>
2023-09-12 19:16:08 -0700

    Fixed pycode

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
 
     bestest = 0
     person = ""
-    if a_dictionary is None
+    if a_dictionary is None:
         return None
     if len(a_dictionary) == 0:
         return None
