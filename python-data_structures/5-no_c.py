30e7e42fca32cdcec3fb203874fa809390481c84
SavageLM <nagol.egavas@gmail.com>
2023-09-12 21:19:41 -0700

    made it executable

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -5,5 +5,5 @@ def no_c(my_string):
     if my_string is not None:
         for i in my_string:
             if my_string[i] != "c" and my_string[i] != "C":
-                new_string.append(my_string[i])
+                new_string.append(my_list[i])
         return new_string
