71b1f8ef50ce89a832419ab0790cab4595c25ba8
SavageLM <nagol.egavas@gmail.com>
2023-09-12 19:14:57 -0700

    Changed an if statement

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,9 @@ def best_score(a_dictionary):
 
     bestest = 0
     person = ""
-    if a_dictionary is None or len(a_dictionary) == 0:
+    if a_dictionary is None
+        return None
+    if len(a_dictionary) == 0:
         return None
     for key in a_dictionary:
         value = a_dictionary.get(key)
