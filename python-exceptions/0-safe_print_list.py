c4b268f50047f5acfcb6404eecf9d33be8085032
SavageLM <nagol.egavas@gmail.com>
2023-09-12 20:47:33 -0700

    Correcting print statement in 0-safe

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     printed = 0
     for i in range(x):
         try:
-            print("{}".format(my_string), end='')
+            print("{}".format(my_string[i]), end='')
             printed += 1
         except Exception:
             pass
