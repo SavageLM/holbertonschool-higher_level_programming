3cbc9cf2289023b86f7f593a0b7d72747e85c0e2
SavageLM <nagol.egavas@gmail.com>
2023-09-12 21:39:09 -0700

    Changed first for loop in file 0 to iterate through a range, not a list

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
 
-    new_matrix = []
-    for i in matrix:
-        new_matrix.append([])
-        for j in i:
-            new_matrix.append(j * j)
-    return new_matrix
+    sqr_matrix = []
+    for i in range(len(matrix)):
+        sqr_matrix.append([])
+        for j in matrix[i]:
+            sqr_matrix.append(j * j)
+    return sqr_matrix
