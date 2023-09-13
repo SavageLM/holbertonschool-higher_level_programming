59ba185963a8a1b950c80e3a03aa26aae8083400
SavageLM <nagol.egavas@gmail.com>
2023-09-12 19:13:38 -0700

    Added 10-best file and code

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+def best_score(a_dictionary):
+
+    bestest = 0
+    person = ""
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+    for key in a_dictionary:
+        value = a_dictionary.get(key)
+        if value > bestest:
+            bestest = value
+            person = key
+    return person
