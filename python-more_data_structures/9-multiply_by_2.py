650a9c8c42578fbd14c26ecc60a26c01c0cd9cde
SavageLM <nagol.egavas@gmail.com>
2023-09-13 10:36:09 -0700

    Fixed spelling mistakes, rewrote update function for new_dict

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
 
-    if a-dictionary is None:
+    if a_dictionary is None:
         return None
     new_dict = {}
     double_val = 0
     for key in a_dictionary:
         value = a_dictionary.get(key)
         double_val = value * 2
-        new_dict[key] = double_val
-    return new_dic
+        new_dict.update({key: double_val})
+    return new_dict
