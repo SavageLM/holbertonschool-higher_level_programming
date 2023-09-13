2222475ed238a4c1b9e9e84456dc5987e0eb2b5c
SavageLM <nagol.egavas@gmail.com>
2023-09-12 17:29:07 -0700

    Removed lambda

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
 
-    power = lambda x, y: x**y
     new_matrix = []
     for i in matrix:
         new_matrix.append([])
         for j in i:
-            new_matrix.append(power(j, 2))
+            new_matrix.append(j * j)
     return new_matrix
