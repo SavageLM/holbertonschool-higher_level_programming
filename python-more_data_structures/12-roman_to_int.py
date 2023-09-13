422887c1400cdafef854be6b893bd1c0d35a3f30
SavageLM <nagol.egavas@gmail.com>
2023-09-13 10:17:22 -0700

    Fixed pycode style

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,9 +5,9 @@ def roman_to_int(roman_string):
         return int(0)
     if type(roman_string) != str:
         return int(0)
-    rome = {'I':1,'V':5,'X':10,'L':50,'C':100,
-            'D':500,'M':1000,'IV':4,'IX':9,'XL':40,
-            'XC':90,'CD':400,'CM':900}
+    rome = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+            'D': 500, 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40,
+            'XC': 90, 'CD': 400, 'CM': 900}
     number = 0
     for i in range(len(roman_string)):
         if i > 0 and rome[roman_string[i]] > rome[roman_string[i - 1]]:
