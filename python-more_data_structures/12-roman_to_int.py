394ac21aa3e85bbf6e3bd96f1c8858fa3ff0c710
SavageLM <nagol.egavas@gmail.com>
2023-09-13 10:14:59 -0700

    Rewrote the for loop in file 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,14 +5,13 @@ def roman_to_int(roman_string):
         return int(0)
     if type(roman_string) != str:
         return int(0)
-    roman = 'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
-    index = 0
+    rome = {'I':1,'V':5,'X':10,'L':50,'C':100,
+            'D':500,'M':1000,'IV':4,'IX':9,'XL':40,
+            'XC':90,'CD':400,'CM':900}
     number = 0
-    while index < len(roman_string):
-        if idx + 1 < len(roman_string) and roman_string[idx:idx = 2] in roman:
-            number += roman[roman_string[idx:idx + 2]]
-            idx += 2
+    for i in range(len(roman_string)):
+        if i > 0 and rome[roman_string[i]] > rome[roman_string[i - 1]]:
+            number += rome[roman_string[i]] - 2 * rome[roman_string[i - 1]]
         else:
-            number += roman[roman_string[idx]]
-            idx += 1
+            number += rome[roman_string[i]]
     return int(number)
