049e9238a592f935470c75221b728a8c96692e82
SavageLM <nagol.egavas@gmail.com>
2023-09-12 21:26:57 -0700

    Restructured 11-delete

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
 
+    new_list = my_list
     if idx < 0:
         return my_list
     if idx > len(my_list):
         return my_list
-    new_list = del my_list[idx]
+    del new_list[idx]
     return new_list
