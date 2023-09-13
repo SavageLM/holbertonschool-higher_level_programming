9fd9129686fe0b2b5086bc773714e1f8039e5eb5
SavageLM <nagol.egavas@gmail.com>
2023-09-12 21:44:33 -0700

    Added an index to append statement in second for loop

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -5,5 +5,5 @@ def square_matrix_simple(matrix=[]):
     for i in range(len(matrix)):
         sqr_matrix.append([])
         for j in matrix[i]:
-            sqr_matrix.append(j * j)
+            sqr_matrix[i].append(j * j)
     return sqr_matrix
