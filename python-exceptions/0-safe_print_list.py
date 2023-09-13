f546f02a273e5a363300a33b5b2d7defbd66c480
SavageLM <nagol.egavas@gmail.com>
2023-09-12 20:56:10 -0700

    Fixed print statement to print my_list, not my_string, added empty print before return

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,8 +4,9 @@ def safe_print_list(my_list=[], x=0):
     printed = 0
     for i in range(x):
         try:
-            print("{}".format(my_string[i]), end='')
+            print("{}".format(my_listi[i]), end='')
             printed += 1
         except Exception:
             continue
+    print()
     return printed
