37ad3b80e1d29e5e3cc71496d75f8168788fb056
SavageLM <nagol.egavas@gmail.com>
2023-09-12 20:50:46 -0700

    Changed pass to continue

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,5 +7,5 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_string[i]), end='')
             printed += 1
         except Exception:
-            pass
+            continue
     return printed
