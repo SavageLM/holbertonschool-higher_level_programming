c5ef80659cf9e1983e89674223fd9d5793b31f28
SavageLM <nagol.egavas@gmail.com>
2023-09-14 12:46:55 -0700

    Fixed pycodestyle, changed size to value in setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -14,16 +14,18 @@ class Square:
             size(int): Size of the square.
         """
         self.__size = size
+
     @property
     def size(self):
         """This retrieves the value of size"""
         return self.__size
+
     @size.setter
     def size(self, value):
         """This sets the value of size"""
-        if type(size) is not int:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
