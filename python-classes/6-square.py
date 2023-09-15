45bc516e2e3c2aec446ca1542c29e64e882c93a3
SavageLM <nagol.egavas@gmail.com>
2023-09-15 11:45:38 -0700

    Added more dunders back

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -14,8 +14,8 @@ class Square:
             size(int): Size of the square.
             position(int, int): position of square.
         """
-        self.size = size
-        self.position = position
+        self.__size = size
+        self.__position = position
 
     @property
     def size(self):
