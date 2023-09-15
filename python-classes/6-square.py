4e0f6d904d8d2331a21a49be233f4465e5bea040
SavageLM <nagol.egavas@gmail.com>
2023-09-15 11:58:39 -0700

    Changed conditionals on position setter

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -37,7 +37,13 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) is not int or value < 0:
+        if type(value) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value [1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
