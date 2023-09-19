733364566e19ddca93f887a6b0f4522b56d43d7b
SavageLM <nagol.egavas@gmail.com>
2023-09-19 10:23:35 -0700

    Add 1-rectangle file and code

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+# 1-rectangle.py
+# Logan Savage <6667@holbertonstudents.com>
+"""Define a class- Rectangle"""
+
+
+class Rectangle:
+    """This is a class for making a Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes a Rectangle.
+
+        Args:
+            self: The rectangle
+            width (int): width of the rectangle
+            height (int): Height of the rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of the Rectangle.
+
+        Args:
+            self: The rectangle
+            value (int): value width will be set to
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the Rectangle.
+
+        Args:
+            self: The rectangle
+            value (int): value width will be set to
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
