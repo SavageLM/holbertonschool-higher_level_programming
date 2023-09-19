11db40b14fdea45ef055d65f2fd595386080be53
SavageLM <nagol.egavas@gmail.com>
2023-09-19 04:51:12 -0700

    Added 6-max_integer unitest file

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# 6-max_integer_test.py
+# Logan Savage
+"""Unit-test functions for max_integer()"""
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+"""importing the function and unitest functions"""
+
+class TestMaxInteger(unitest.TestCase):
+    """ Testing maz_integer"""
+
+    def test_empty_list(self):
+        """ Tests for empty lists"""
+        no_list = []
+        self.assertEqual(max_integer(empty_list), None)
+
+    def test_empty_string(self):
+        """ Tests for empty strings"""
+        self.assertEqual(max_integer(""), None)
+
+    def test_unsorted_list(self):
+        """ Tests for unsorted lists"""
+        unsort = [1, 9, 3, 6]
+        self.assertEqual(max_integer(unsort))
+
+    def test_sorted_list(self):
+        """ Tests for sorted lists"""
+        sort = [1, 2, 6, 9]
+        self.assertEqual(max_integer(sort))
+
+if __name__ == '__main__':
+    unittest.main()
