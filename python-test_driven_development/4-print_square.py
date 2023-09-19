675fcd26836cf01951025b469236e0bfc358ac73
SavageLM <nagol.egavas@gmail.com>
2023-09-19 02:50:36 -0700

    Forgot a colon

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -13,7 +13,7 @@ def print_square(size):
     """
     if type(size) is not int:
         raise TypeError("size must be an integer")
-    elif size < 0
+    elif size < 0:
         raise ValueError("size must be >= 0")
     else:
         if size == 0:
