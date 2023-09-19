7a73eb01ab83408032f3039384c6cc933f4fad2a
SavageLM <nagol.egavas@gmail.com>
2023-09-19 02:44:06 -0700

    Added 4-print_square test file and code

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# 4-print_square.py
+# Logan Savage <6667@holbertonstudents.com>
+""" Defines a function that prints a square """
+
+
+def print_square(size):
+    """Prints a square of size
+
+    Raises:
+    TypeError if size is not an int
+    ValueError if size is not positive
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0
+        raise ValueError("size must be >= 0")
+    else:
+        if size == 0:
+            print()
+        for i in range(size):
+            for j in range(size):
+                print("#", end='')
+            print()
