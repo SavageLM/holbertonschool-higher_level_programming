b5f2689c5e78157506c4e7c8d9beef45695cd0e6
SavageLM <nagol.egavas@gmail.com>
2023-09-19 09:56:17 -0700

    Added a new directory, and 0-rectangle file and code

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Logan Savage <6667@holbertonstudents.com>
+"""Define a class- Rectangle"""
+
+class Rectangle:
+    """This is a class for making a Rectangle"""
+    pass
