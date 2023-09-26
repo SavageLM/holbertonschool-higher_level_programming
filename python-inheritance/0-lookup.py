0371243787ff2ceffe4cbc5f1fc413ed75bf7d86
SavageLM <nagol.egavas@gmail.com>
2023-09-25 19:44:34 -0700

    Added 0-lookup and code

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 0-lookup.py
+# Logan Savage <6667@holbertonstudents.com>
+""" Defines a function that retrieves all attributes and methods of an object"""
+
+def lookup(obj):
+    """Returns a list of attributes and methods of obj
+
+    Args:
+        obj (any): object to retirieve from
+    """
+    return list(dir(obj))
---
+++ python-inheritance/README.md
@@ -0,0 +1,1 @@
+This is a readme for the inheritance python project at Holberton.
