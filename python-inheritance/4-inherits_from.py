97c054cc7cad7597dc1e907f9ad49da7d0f555b3
SavageLM <nagol.egavas@gmail.com>
2023-09-25 22:16:54 -0700

    misspelled class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -11,7 +11,7 @@ def inherits_from(obj, a_class):
         obj (any): object to determine
         a_class (any): class to compare to.
     """
-    if issubclas(type(obj), a_class) and type(obj) is not a_class:
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
         return False
