feb1c5a856197458bbdafb823af280b568adde94
SavageLM <nagol.egavas@gmail.com>
2023-09-25 22:20:02 -0700

    Added 5-base

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Logan Savage <6667@holbertonstudents.com>
+"""Defines a class, BaseGeometery"""
+
+
+class BaseGeometery:
+    """An empty class of BaseGeometery"""
+    pass
