98b1dfeb60b8d429182b631e35e3e32210ff104a
SavageLM <nagol.egavas@gmail.com>
2023-09-25 22:24:08 -0700

    Added 6-base

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+# Logan Savage <6667@holbertonstudents.com>
+"""Defines a class, BaseGeometry"""
+
+
+class BaseGeometry:
+    """A class of BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
