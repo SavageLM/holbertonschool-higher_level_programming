2db831c25ec789043e34c7ef242d8fa394c45a20
SavageLM <nagol.egavas@gmail.com>
2023-09-26 02:20:45 -0700

    Added file 10-square and code

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# 10-square.py
+# Logan Savage <6667@holbertonstudents.com>
+"""Defines a class, Square, using BaseGeometry"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class Square that inherits Rectangle"""
+
+    def __init__(self, size):
+        """ Initializes an object of class Rectangle.
+
+        Args:
+            size (int): Size of the square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
