eb043251d118b72372a3ae93ee58024d65410a97
SavageLM <nagol.egavas@gmail.com>
2023-09-26 18:11:55 -0700

    Fixed typos

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,7 +5,7 @@
 import json
 import sys
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     save_json = __import__('5-save_to_json_file').save_to_json_file
     load_json = __import__('6-load_from_json_file').load_from_json_file
 
@@ -13,5 +13,5 @@ try:
     arg_list = load_from_json_file("add_item.json")
 except FileNotFoundError:
     arg_list = []
-arg_list.exted(sys.argv[1:])
+arg_list.extend(sys.argv[1:])
 save_jason(arg_list, "add_item.json")
