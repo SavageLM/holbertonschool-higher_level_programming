2c6224db747c42d449aa27c8b0cc31661f99854e
SavageLM <nagol.egavas@gmail.com>
2023-09-26 19:49:35 -0700

    Added 10-studet

---
+++ python-input_output/10-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# 10-student.py
+# Logan Savage <6667@holbertonstudents.com>
+"""Defines a class of Student"""
+
+
+class Student:
+    """A class of Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes new object Student
+
+        Args:
+            first_name (str): First name of student
+            last_name (str): Last name of Student
+            age (int): Age of student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns dictionary representation of a student
+        
+        If attrs is a list of strings, only attributes will be included
+
+        Args:
+            attrs (list): Attributes to include
+        """
+        if type(attrs) is list and all(type(ele) == str for ele in attrs)):
+            return {idx: getattr(self, idx)
+                    for idx in attrs if hasattr(self,idx)}
+        return self.__dict__
