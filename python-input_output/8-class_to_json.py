0f4efd384cf931d0b10f28566edb9feba487fade
SavageLM <nagol.egavas@gmail.com>
2023-09-26 19:12:32 -0700

    Moving on to 8-class file and code

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+# 8-class_to_json.py
+# Logan Savage <6667@holbertonstudents.com>
+"""Defines a function that converts class to JSON"""
+
+
+def class_to_json(obj):
+    """Returns dictionary description of a simple structure"""
+    return obj.__dict__
