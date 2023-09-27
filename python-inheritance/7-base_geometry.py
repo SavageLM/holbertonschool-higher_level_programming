06ff585b2bc1184286183bbb830b90c07bd3fb9d
SavageLM <nagol.egavas@gmail.com>
2023-09-26 20:37:25 -0700

    Try defining area on 7-base

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -8,6 +8,7 @@ class BaseGeometry:
     """A class of BaseGeometry"""
 
     def area(self):
+        """Returns the area when implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
@@ -17,7 +18,7 @@ class BaseGeometry:
             name (str): name of value
             value (int): value to check
         """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
