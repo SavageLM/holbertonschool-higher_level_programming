c1aee576ba04d666cd7fa25f99ae44f9b46b6e1f
SavageLM <nagol.egavas@gmail.com>
2023-09-26 19:31:12 -0700

    Added file 9-student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -7,4 +7,18 @@
 class Student:
     """A class of Student"""
 
-    def __init(self, first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
+        """Initializes new object Student
+
+        Args:
+            first_name (str): First name of student
+            last_name (str): Last name of Student
+            age (int): Age of student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns dictionary representation of a studnet"""
+        return self.__dict__
