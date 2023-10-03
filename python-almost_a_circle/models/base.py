725099234fffada8d5c3fd7bae17cce835ba11cf
SavageLM <nagol.egavas@gmail.com>
2023-10-03 08:17:07 -0700

    Added to_json_string to base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,3 +18,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Function that Returns the JSON string for a list of dictionaries"""
+        if len(list_dictionaries) is 0 or list_dictionaries is None:
+            return []
+        else:
+            return json.dumps(list_dictionaries)
