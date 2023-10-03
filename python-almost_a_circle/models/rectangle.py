f764ea1fd86acfcda4f3529334e59500eb235da7
SavageLM <nagol.egavas@gmail.com>
2023-10-03 03:53:19 -0700

    Added to_dictionary function to rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -149,3 +149,13 @@ class Rectangle(Base):
                     self.x = value
                 elif key == 'y':
                     self.y = value
+
+    def to_dictionary(self):
+        """Returns a dictionary of Rectangle"""
+        return {
+                "id": self.id,
+                "width": self.width
+                "height": self.height
+                "x": self.x
+                "y": self.y
+                }
