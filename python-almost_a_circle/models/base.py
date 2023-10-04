2d2c0468d53a5fbed9ab49487c06a94fa3287dc6
SavageLM <nagol.egavas@gmail.com>
2023-10-04 12:28:33 -0700

    changed is to ==

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -30,7 +30,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """Function for converting a json string into a list"""
-        if json_string is None or len(json_string) is 0:
+        if json_string is None or len(json_string) == 0:
             return "[]"
         else:
             return json.loads(json_string)
