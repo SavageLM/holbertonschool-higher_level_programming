19de39bee7c519ec1c617b6ac5824ea67dbe2f7e
SavageLM <nagol.egavas@gmail.com>
2023-10-23 16:22:36 -0500

    Made dir, added README, added file 0-select and code

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This is a script for listing all states in a database, hbtn_0e_usa"""
+
+import MySQLdb
+import sys
+
+
+def all_states():
+    """ Finds a nd prints all states from database"""
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database)
+    dc = db.cursor()
+    dc.execute('SELECT * FROM states ORDER BY id ASC')
+    results = dc.fetchall()
+    for state in results:
+        print(state)
+    dc.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    all_states()
