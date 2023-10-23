3e54d6a2ca4853d354fb4e914e5a6cb24efce888
SavageLM <nagol.egavas@gmail.com>
2023-10-23 13:47:09 -0500

    Rearranging code

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- Creates a database and Table with constraints.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL
     name VARCHAR(256) NOT NULL
     FOREIGN KEY(state_id) REFERENCES states(id)
