1ec0bb467821be836fc844cadd63b81787db9705
SavageLM <nagol.egavas@gmail.com>
2023-11-27 18:18:29 -0600

    Fixed semistandard and chmod

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const myVar1 = 'C is fun'
-const myVar2 = 'Python is cool'
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
 const myVar3 = 'JavaScript is amazing';
 function printStrings (myString1, myString2, myString3) {
   console.log(myString1);
