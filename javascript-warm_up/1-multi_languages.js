da5ac262270d7832dfe65fb744f897f02bf2eaef
SavageLM <nagol.egavas@gmail.com>
2023-11-27 18:07:46 -0600

    Added 1-multi_languages file and code

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool'
+const myVar3 = 'JavaScript is amazing';
+function printStrings (firstString, secondString, thrirdString) {
+  console.log(firstString);
+  console.log(secondString);
+  console.log(thrirdString);
+}
+printStrings(myVar1, myVar2, myVar3);
