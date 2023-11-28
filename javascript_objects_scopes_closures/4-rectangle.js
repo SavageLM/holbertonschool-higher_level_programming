2350e6aeda11ae71e5414e5705c8d2593e49fbb6
SavageLM <nagol.egavas@gmail.com>
2023-11-28 03:32:17 -0600

    Fixed semistandard in 4-rectangle

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,31 +1,30 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        let row = '';
-        for (let j = 0; j < this.width; j++) {
-          row += 'X';
-        }
-        console.log(row);
-      }
-    }
-
-    rotate() {
-        [this.height, this.width] = [this.width, this.height]
-    }
-
-    double() {
-        this.width = this.width * 2;
-        this.height = this.height * 2;
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  module.exports = Rectangle;
-  
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
