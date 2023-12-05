1f66f1f25900e0ee8a4461ee8fa6db43473f91c9
SavageLM <nagol.egavas@gmail.com>
2023-12-04 19:55:13 -0600

    Maybe it was content that was undefined

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,10 +2,8 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
-fs.writeFile(filePath, content, (error, content) => {
+fs.writeFile(filePath, content, (error) => {
   if (error) {
     console.log(error);
-  } else {
-    console.log(content);
   }
 });
