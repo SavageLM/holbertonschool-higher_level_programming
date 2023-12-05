3f28e2e342b87edb51eece13ac14f75853708377
SavageLM <nagol.egavas@gmail.com>
2023-12-04 19:53:27 -0600

    Added if statement to handle error

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,5 +3,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 fs.writeFile(filePath, content, (error, content) => {
-  console.log(error || content);
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(content);
+  }
 });
